dc0720c239e40a26cd47467188ec6ca94eedb524
Jasmine Singleton <126820711+JasmineSingleton@users.noreply.github.com>
2024-11-19 12:12:24 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -13,27 +13,52 @@ $(document).ready(function() {
       zoom += 0.1;
       $('body').css('zoom', zoom);
     }
+  
+  $("*:not(body)").hover(
+    function (ev) {
 
+      $(this).addClass("highlight");
 
+      var spokenWord = $(this).text();
+      var altText = $(this).attr("alt")
+      var srcofimg = $(this).attr("src");
 
-  $(document).keydown(function(event) {
-    if (event.code === 'Space') {
-      event.preventDefault(); // stops page from jumping when spacebar is pressed
+      if (spokenWord){ // Speaks the text 
+        speechSynthesis.speak(new SpeechSynthesisUtterance(spokenWord));
+        }
 
-      if (!large && magnify) { // making sure nothing is enlarged
-        const thisEnlarge = $(magnify).html(); // getting the HTML
+      else if (altText){ // Speaks the alt text if there is some
+        speechSynthesis.speak(new SpeechSynthesisUtterance(altText));
+        }
 
-        
-        large = $('<div></div>'); // creating a new div element
-        large.attr('id', 'large'); 
-        large.html(thisEnlarge); // content from what is selected 
+      else if (srcofimg){  // Speaks the image source if there is no alt text or spokenWord
+        speechSynthesis.speak(new SpeechSynthesisUtterance(srcofimg));
+        }
+      },
 
-        $('body').append(large);
-      } else if (large) { 
-        large.remove(); // Remove the enlarged content 
-        large = null; 
+    function () {
+      $(this).removeClass("highlight");
+        speechSynthesis.cancel();
+
       }
-    }
+    );
+    document.addEventListener('keydown', function (e) {
+
+      if (e.code == 'Space' ) { // Checking for spacebar and stopping the page from jumping
+          e.preventDefault(); 
+
+          if (speechSynthesis.speaking) { // Canceling the speech synthesis if it's on
+              speechSynthesis.cancel();
+      
+          } else {
+              speechSynthesis.speak(new SpeechSynthesisUtterance($(this).text()));
+          }
+
+      } else {
+          
+          speechSynthesis.cancel();
+          $(".highlight").removeClass("highlight");
+      }
+    });
   });
-});
 });
